aff42f5e9b2c60f1abb7ef427686648cc3860ab9
gehelem <gilles@joag.fr>
2016-11-01 19:45:18 +0100

    Cleaning code

---
+++ build/CMakeFiles/cam84.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+FILE(REMOVE_RECURSE
+  "libcam84.a"
+)
---
+++ build/config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/* Define INDI Data Dir */
+#define INDI_DATA_DIR "/usr/share/indi"
+/* Define Driver version */
+#define CAM84_VERSION_MAJOR 1
+#define CAM84_VERSION_MINOR 0
+
+#endif // CONFIG_H
